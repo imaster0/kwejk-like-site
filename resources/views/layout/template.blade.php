5cba4fa168b4e8478139ef86cc0c95e4c1cc3051
Jakub Szczugieł <imaster0x@gmail.com>
2017-08-22 20:46:46 +0200

    Some corrections 3/5

--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -13,26 +13,13 @@
 	    <link href="{{ asset('vendor/font-awesome/css/font-awesome.min.css') }} " rel="stylesheet" type="text/css">
       <link rel="stylesheet" href="{{ asset('vendor/font-awesome/css/font-awesome.css') }}" type="text/css">
 	    <link rel="stylesheet" href="{{ asset('css/custom.css') }}" type="text/css">
-      <!-- meta fb tags -->
-      <!-- <meta property="og:url"                content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html" />
-      <meta property="og:type"               content="article" />
-      <meta property="og:title"              content="When Great Minds Don’t Think Alike" />
-      <meta property="og:description"        content="How much does culture influence creative thinking?" />
-      <meta property="og:image"              content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg" /> -->
+      <!-- fb metas -->
+      <meta property="og:type"          content="article" />
+      <meta property="og:title"         content="Dowiedz się więcej" />
+      <meta property="og:description"   content="Lajfhaki, pomysły, inspiracje - wszystko to w formie krótkich memów!" />
 
     </head>
     <body>
-      <!-- <div id="fb-root"></div>
-      <script>(function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/pl_PL/sdk.js#xfbml=1&version=v2.10";
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));</script>
- -->
-
-
 
   		<div id="wrapper">
   			<!-- MENUBAR -->
