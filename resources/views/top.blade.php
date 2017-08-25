fcf8e0cb7ee83386ffa5372138f3eac125124a4b
Jakub Szczugieł <imaster0x@gmail.com>
2017-08-25 13:15:02 +0200

    Top fix

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -13,6 +13,6 @@
 <?php
 if($tag =='all')  $posts = App\Post::where('verified', true)->orderByRaw('(up - down) desc')->orderBy('up', 'desc')->orderBy('created_at', 'desc')->paginate(10);
 
-else $posts = App\Tag::where('name', $tag)->first()->posts()->where('verified', true)->orderByRaw('(up - down) desc')->orderBy('up', 'desc')->orderBy('created_at', 'desc')->paginate(10);->paginate(10);
+else $posts = App\Tag::where('name', $tag)->first()->posts()->where('verified', true)->orderByRaw('(up - down) desc')->orderBy('up', 'desc')->orderBy('created_at', 'desc')->paginate(10);
 ?>
 @endsection
